b0aa781ac65eed72973c8b81f308c58bc7cdef21
Ben Hsieh <xeiyan@gmail.com>
2016-11-01 10:10:52 +0800

    Add ground overlay

--- index.js
+++ index.js
@@ -5,6 +5,7 @@ var app = express();
 
 app.set('port', (process.env.PORT || 5000));
 
+app.use('/public', express.static('public'));
 app.listen(app.get('port'), function() {
   console.log('Node app is running on port', app.get('port'));
 });
